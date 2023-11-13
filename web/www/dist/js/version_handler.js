5601ee4c203d38d035bc44b598ba435fd5d77835
CosmicStar98 <58494315+CosmicStar98@users.noreply.github.com>
2023-11-12 20:45:04 -0500

    fix readme version not displaying

--- web/www/dist/js/version_handler.js
+++ web/www/dist/js/version_handler.js
@@ -1,13 +1,13 @@
 $(document).ready(() => {
 	if (window.location.pathname == "/" || "/index.html" || "/index.htm") {
-		$.getJSON("./dist/json/version.json", (result) => {
+		$.get(window.location.origin + "/dist/json/version.json", (result) => {
 			$.each(result, function (i, field) {
 				$("#login_version").text(`Version ${field}`);
 				$("#ver_log").text(`Version ${field}`);
 			});
 		});
 	} else {
-		$.getJSON("../../dist/json/version.json", (result) => {
+		$.get(window.location.origin + "/dist/json/version.json", (result) => {
 			$.each(result, function (i, field) {
 				$(".ver").text(`BonziWORLD Enhanced  v${field} `);
 			});
